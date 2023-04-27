6624c6fec968d30f36e94aa163682c933c1c5bed
abdo <mr.spykhattab@gmail.com>
2023-04-28 02:07:44 +0300

    Now Two players can play against each others

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 500
 PERIOD = 10  # ms
 
-playerScore = 0
-pcScore = 0
+player2Score = 0
+player1Score = 0
 
 deltaX = 1
 deltaY = -1
@@ -19,12 +19,14 @@ FROM_TOP = 3
 FROM_BOTTOM = 4
 
 mouse_x = 400
+keyboard_x = 400
 
 ball = rect(390, 240, 410, 260)
 wall = rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
-player = rect(0, 0, 80, 20)
+player1 = rect(0, 0, 80, 20)
+player2 = rect(0, 480, 80, 500)
 
-r, g, b = 1, 1, 1
+curr_color = 1
 
 
 def Init_Camera_Proj():
@@ -38,34 +40,39 @@ def Init_Camera_Proj():
 
 
 def display():
-    global deltaX, deltaY, playerScore, pcScore
+    global deltaX, deltaY, player2Score, player1Score, curr_color
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glColor3f(1, 1, 1)
-    draw_rect(ball)
 
     glPushMatrix()
-    s1 = "PC : " + str(pcScore)
+    s1 = "Player1 : " + str(player1Score)
     draw_text(s1, 10, 460)
     glPopMatrix()
 
     glPushMatrix()
-    s2 = "Player : " + str(playerScore)
+    s2 = "Player2 : " + str(player2Score)
     draw_text(s2, 10, 420)
     glPopMatrix()
 
-    player.left = mouse_x - 30
-    player.right = mouse_x + 30
-    glColor3f(1, 1, 1)
-    draw_rect(player)
+    glColor3f(1, curr_color, curr_color)
+    draw_rect(ball)
+    player1.left = mouse_x - 40
+    player1.right = mouse_x + 40
+    draw_rect(player1)
 
-    if test_plyer_ball(ball, player):
+    if test_player1_ball(ball, player1):
+        curr_color = 0
         deltaY = 1
-        playerScore += 1
+        player1Score += 1
+
+    if test_player2_ball(ball, player2):
+        curr_color = 0
+        deltaY = -1
+        player2Score += 1
 
     # Collsion detection between Wall and Ball
     if test_wall_ball(ball, wall) == FROM_BOTTOM:
         deltaY = 1
-        pcScore += 1
+        player1Score += 1
     if test_wall_ball(ball, wall) == FROM_LEFT:
         deltaX = 1
     if test_wall_ball(ball, wall) == FROM_RIGHT:
@@ -73,11 +80,17 @@ def display():
     if test_wall_ball(ball, wall) == FROM_TOP:
         deltaY = -1
 
+    player2.left = keyboard_x - 40
+    player2.right = keyboard_x + 40
+    draw_rect(player2)
+    print(keyboard_x)
+
     ball.left += deltaX
     ball.right += deltaX
     ball.bottom += deltaY
     ball.top += deltaY
-
+    if curr_color < 1:
+        curr_color += 0.01
     glutSwapBuffers()
 
 
@@ -93,7 +106,7 @@ def test_wall_ball(ball: rect, wall: rect):
     return None
 
 
-def test_plyer_ball(ball: rect, player: rect):
+def test_player1_ball(ball: rect, player: rect):
     if ball.bottom == player.top and ball.left >= player.left and ball.right <= player.right and deltaY == -1:
         return True
     if ball.bottom == player.top and ball.left < player.left and ball.right >= player.left and deltaY == -1:
@@ -103,11 +116,17 @@ def test_plyer_ball(ball: rect, player: rect):
     return False
 
 
+def test_player2_ball(ball: rect, player: rect):
+    if ball.top == player.bottom and ball.left >= player.left and ball.right <= player.right and deltaY == 1:
+        return True
+    if ball.top == player.bottom and ball.left < player.left and ball.right >= player.left and deltaY == 1:
+        return True
+    if ball.top == player.bottom and ball.right > player.right and ball.left <= player.right and deltaY == 1:
+        return True
+    return False
+
+
 def draw_text(string, x, y):
-    """
-    string : is the str to be drawn
-    x , y : are shift 
-    """
     glColor3f(1, 1, 0)
     glLineWidth(2)
     glTranslate(x, y, 0)
@@ -118,11 +137,11 @@ def draw_text(string, x, y):
 
 
 def keyboard(key, x, y):
-    """
-    key : hitted key on keyboard 
-    x : x postion of mouse on window
-    y : y postion of mouse on window
-    """
+    global keyboard_x
+    if key == b"a":
+        keyboard_x = max(0, keyboard_x - 10)
+    if key == b"d":
+        keyboard_x = min(keyboard_x + 10, WINDOW_WIDTH)
     if key == b"q":
         sys.exit(0)
 
